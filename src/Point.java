e6f478a87cda71752825c66211871a3565037caf
2cut <jialiangc@nycstudents.net>
2022-10-03 19:42:37 -0400

    finished lab

--- src/Point.java
+++ src/Point.java
@@ -1,17 +1,22 @@
 public class Point {
-    private int x;
-    private int y;
+    int x;
+    int y;
 
     public Point(int x, int y) {
-        x = this.x;
-        y = this.y;
+        this.x = x;
+        this.y = y;
     }
 
     public Point(int x) {
-        x = this.x;
-        y = this.x;
+        this.x = x;
+        this.y = y;
     }
 
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+    
     public int getX() {
         return x;
     }
@@ -21,21 +26,31 @@ public class Point {
     }
 
     public void setX(int x) {
-        x = this.x;
+        this.x = x;
     }
     public void setY(int y) {
-        y = this.y;
+        this.y = y;
     }
 
     public String coordinate() {
         return ("("+x+", "+y+")");
     }
 
-    public int quadrant() {
+    public String quadrant() {
         if (x>0 && y>0) {
-        } else {
-
+            return ("I");
+        } else if (x>0 && y<0) {
+            return ("IV");
+        } else if (x<0 && y>0) {
+            return ("II");
+        } else if (x<0 && y<0) {
+            return ("III");
+        } else if (x==0 && y==0) {
+            return ("origin");
+        } else if (x==0 || y==0) {
+            return ("on an axis");
         }
+        return null;
     }
 
 }
